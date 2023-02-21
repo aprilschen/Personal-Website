7c45204b6e11eb42b872f39e5af096500df19657
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-21 15:53:34 -0800

    Loading Screen

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,10 +4,36 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 import { Box } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import ReactLoading from "react-loading";
+import { Flex } from '@chakra-ui/react'
 
 function App() {
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(
+          setDone(true)
+        );
+    }, 2000);
+  }, []);
+
+
   return (
     <div className="App">
+    {!done ? (
+      <Flex minHeight={'100vh'} justify={'center'} align={'center'}>
+        <ReactLoading
+        type={"cubes"}
+        color={"#FC6AAF"}
+        height={'15%'}
+        width={'15%'}
+        />
+      </Flex>
+
+    ) : (
       <Box>
         <Landing/>
         <Navbar/>
@@ -15,9 +41,9 @@ function App() {
         <Projects/>
         <Footer/>
       </Box>
-
+    )}
     </div>
-  )
+  );
 }
 
 export default App
