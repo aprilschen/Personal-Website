d1f844287b8f54d68c2a7045059566439fe2284a
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-21 14:08:10 -0800

    Configured Navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,9 +1,56 @@
-function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
-}
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Container,
+    Flex,
+    Spacer,
+    HStack,
+    IconButton,
+    useBreakpointValue,
+  } from '@chakra-ui/react'
+  import { FiMenu } from 'react-icons/fi'
+  //import { Logo } from './Logo'
+  import Photo from '../assets/react.svg'
 
-export default Navbar
+  export default function App () {
+    const isDesktop = useBreakpointValue({
+      base: false,
+      lg: true,
+    })
+
+    return (
+      <Box
+        as="section"
+        pb={{
+          base: '12',
+          md: '24',
+        }}
+      >
+        <Box boxShadow="sm"
+            py={{
+              lg: '5',
+            }} px={{sm:'10'}}>
+            <Flex>
+              <img src={Photo} />
+              <Spacer/>
+              {isDesktop ? (
+                  <ButtonGroup variant="link" spacing="8">
+
+                    <Button key='About'>About</Button>
+                    <Button key='About'>Portfolio</Button>
+                    <Button key='About'>Contact</Button>
+
+                  </ButtonGroup>
+              ) : (
+                <IconButton
+                  variant="ghost"
+                  icon={<FiMenu fontSize="1.25rem" />}
+                  aria-label="Open Menu"
+                />
+              )}
+            </Flex>
+        </Box>
+      </Box>
+    )
+  }
